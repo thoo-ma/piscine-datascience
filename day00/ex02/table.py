ce74fa8a21187ccd4f98619373f67df9b385f075
thoo-ma <trobin@student.42.fr>
2024-06-01 19:19:17 +0200

    read/write by chunks

--- day00/ex02/table.py
+++ day00/ex02/table.py
@@ -3,60 +3,38 @@ import pandas as pd
 import os
 
 
-csv = '/sgoinfre/goinfre/Perso/trobin/piscine-datascience/subject/customer/data_2022_oct.csv'
-
-
-# Step 1: open csv
-try:
-    df = pd.read_csv(csv, on_bad_lines='warn', parse_dates=['event_time'])
-    # NOTE This is not asked by the subject.
-    # (Even more, this is not recommended since we asked to remove duplicates
-    # once all data_ tables have been merged into the customers one.)
-    # We do it anyway because there is so much lines, and compute all
-    # those useless duplicated would be -- and has been -- very painful.
-    # Even less painful, would be to remove duplicates from the file
-    # directly, avoiding loading duplicated lines into the DataFrame.
-    # GNU `sort` and `uniq` would do the trick.
-    # However, this is more convient to just `drop_duplicates()`.
-    df.drop_duplicates(inplace=True)
-except Exception as e:
-    print(e)
-    exit()
+csv = '/home/youngbenjaminhorne/42/piscine-datascience/subject/customer/data_2022_oct.csv'
 
 engine = sqlalchemy.create_engine("postgresql://trobin:mysecretpassword@localhost:5432/piscineds")
 
 with engine.connect() as connection:
 
-    # Step 2: create type
-    stmt = sqlalchemy.text("CREATE TYPE event_type AS ENUM ('view', 'cart', 'remove_from_cart', 'purchase');")
-    connection.execute(stmt)
+    # Step 1: create type
+    statement = sqlalchemy.text("CREATE TYPE event_type AS ENUM ('view', 'cart', 'remove_from_cart', 'purchase');")
+    connection.execute(statement)
     connection.commit()
 
-    # Step 3: create table
+    # Step 2: create table
     # TODO
     # https://stackoverflow.com/q/58220421
     # https://stackoverflow.com/q/68219690
     table = os.path.basename(csv).split('.')[0]
-    stmt = sqlalchemy.text(f"""
+    statement = sqlalchemy.text(f"""
         CREATE TABLE {table} (
-            event_time TIMESTAMP,
+            event_time TIMESTAMPTZ,
             event_type event_type,
             product_id INTEGER,
             price REAL,
             user_id BIGINT,
             user_session VARCHAR(50));""")
-    connection.execute(stmt)
+    connection.execute(statement)
     connection.commit()
+    print(f'created table {table}')
 
-    # Step 4: fill table
-    df.to_sql(name=table, con=connection, if_exists='append', index=False)
-
-    # Step 4 (alternative)
-    # NOTE `csv` needs to be a valid path in the container filesystem
-    # stmt = sqlalchemy.text(f"""
-    #     COPY {table}(event_time, event_type, product_id, price, user_id, user_session)
-    #     FROM '{csv}'
-    #     DELIMITER ','
-    #     CSV HEADER;""")
-    # connection.execute(stmt)
-    # connection.commit()
+    # Step 3: fill table
+    chunksize = 100000
+    for chunk in pd.read_csv(csv, chunksize=chunksize, on_bad_lines='warn', parse_dates=['event_time']):
+        chunk.drop_duplicates(inplace=True) # NOTE: is it necessay at this point
+        chunk.to_sql(name=table, con=connection, if_exists='append', index=False)
+        print(f'added chunk (100k rows) to table {table}')
+    print(f'filled table {table}')
--- day00/ex03/automatic_table.py
+++ day00/ex03/automatic_table.py
@@ -4,9 +4,9 @@ import os
 
 
 csvs = [
-    '/sgoinfre/goinfre/Perso/trobin/piscine-datascience/subject/customer/data_2022_nov.csv',
-    '/sgoinfre/goinfre/Perso/trobin/piscine-datascience/subject/customer/data_2022_dec.csv',
-    '/sgoinfre/goinfre/Perso/trobin/piscine-datascience/subject/customer/data_2023_jan.csv'
+    '/home/youngbenjaminhorne/42/piscine-datascience/subject/customer/data_2022_nov.csv',
+    '/home/youngbenjaminhorne/42/piscine-datascience/subject/customer/data_2022_dec.csv',
+    '/home/youngbenjaminhorne/42/piscine-datascience/subject/customer/data_2023_jan.csv'
 ]
 
 engine = sqlalchemy.create_engine("postgresql://trobin:mysecretpassword@localhost:5432/piscineds")
@@ -15,31 +15,25 @@ with engine.connect() as connection:
 
     for csv in csvs:
 
-        # Step 1: open csv
-        try:
-            df = pd.read_csv(csv, on_bad_lines='warn', parse_dates=['event_time'])
-            print(f'open {csv}')
-            # NOTE Same rationale for droping duplicates than previous exercise.
-            df.drop_duplicates(inplace=True)
-        except Exception as e:
-            print(e)
-            continue
-
-        # Step 2: create table
+        # Step 1: create table
         table = os.path.basename(csv).split('.')[0]
-        stmt = sqlalchemy.text(f"""
+        statement = sqlalchemy.text(f"""
             CREATE TABLE {table} (
-                event_time TIMESTAMP,
+                event_time TIMESTAMPTZ,
                 event_type event_type,
                 product_id INTEGER,
                 price REAL,
                 user_id BIGINT,
                 user_session VARCHAR(50));""")
-        connection.execute(stmt)
+        connection.execute(statement)
         connection.commit()
-        print(f'create table {table}')
+        print(f'created table {table}')
 
-        # Step 3: fill table
-        df.to_sql(name=table, con=connection, if_exists='append', index=False)
-        print(f'fill table {table}')
+        # Step 2: fill table
+        chunksize = 100000
+        for chunk in pd.read_csv(csv, chunksize=chunksize, on_bad_lines='warn', parse_dates=['event_time']):
+            chunk.drop_duplicates(inplace=True) # NOTE: is it necessary at this point
+            chunk.to_sql(name=table, con=connection, if_exists='append', index=False)
+            print(f'added chunk (100k rows) to table {table}')
+        print(f'filled table {table}')
 
